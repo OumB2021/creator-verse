6f0a042c18aed0cf401b5872f4bd535316827893
Oumb2021 <baarryoumar@gmail.com>
2025-07-31 23:25:22 -0400

    boilerplate set up

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const port = 5000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
